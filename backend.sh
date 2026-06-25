683e2ba8c0069285b8d21ced44b148d32599e87a
aikdp <kdprasad028@gmail.com>
2026-06-25 21:39:14 +0530

    expense project with shell scriting - backend

--- backend.sh
+++ backend.sh
@@ -40,54 +40,54 @@ VALIDATE(){
     fi    
 }
 
-dnf module disable nodejs -y
-VALIDATE $? "Disable Nodejs package"
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disable Nodejs package" | tee -a $LOG_FILE
 
-dnf module enable nodejs:20 -y
-VALIDATE $? "Enabling NodeJS 20"
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling NodeJS 20" | tee -a $LOG_FILE
 
-dnf install nodejs -y
-VALIDATE $? "Installing NodeJS"
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing NodeJS" | tee -a $LOG_FILE
 
-id expense
+id expense &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-    echo "User expense is not exists, create user"
-    useradd expense
+    echo "User expense is not exists, create user" | tee -a $LOG_FILE
+    useradd expense &>>$LOG_FILE
 else
-    echo "user expensse already exists, SKIPPING"
+    echo "user expensse already exists, SKIPPING" | tee -a $LOG_FILE
 fi
 
-mkdir -p /app
+mkdir -p /app &>>$LOG_FILE
 
-rm -rf /app/*
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
-VALIDATE $? "Downloading backend applciation"
+rm -rf /app/* &>>$LOG_FILE
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
+VALIDATE $? "Downloading backend applciation" | tee -a $LOG_FILE
 
-cd /app
-unzip /tmp/backend.zip
-VALIDATE $? "Extracting Backend app file"
+cd /app &>>$LOG_FILE 
+unzip /tmp/backend.zip &>>$LOG_FILE
+VALIDATE $? "Extracting Backend app file" | tee -a $LOG_FILE
 
-npm install
-VALIDATE $? "NPM installtion"
+npm install &>>$LOG_FILE
+VALIDATE $? "NPM installtion" | tee -a $LOG_FILE
 
-cp /home/ec2-user/shell_expense_practice/backend.service /etc/systemd/system/backend.service
-VALIDATE $? "Copying backend service file"
+cp /home/ec2-user/shell_expense_practice/backend.service /etc/systemd/system/backend.service &>>$LOG_FILE
+VALIDATE $? "Copying backend service file" | tee -a $LOG_FILE
 
-dnf install mysql -y
-VALIDATE $? "mysql client installation"
+dnf install mysql -y &>>$LOG_FILE
+VALIDATE $? "mysql client installation" | tee -a $LOG_FILE
 
-mysql -h mysql.telugudevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
-VALIDATE $? "Adding schema to MySQL DB"
+mysql -h mysql.telugudevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
+VALIDATE $? "Adding schema to MySQL DB" | tee -a $LOG_FILE
 
-systemctl daemon-reload
-VALIDATE $? "Deamon reloading"
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "Deamon reloading" | tee -a $LOG_FILE
 
-systemctl enable backend
-VALIDATE $? "Enabling Backend service"
+systemctl enable backend &>>$LOG_FILE
+VALIDATE $? "Enabling Backend service" | tee -a $LOG_FILE
 
-systemctl restart backend
-VALIDATE $? "Restarting Backend"
+systemctl restart backend &>>$LOG_FILE
+VALIDATE $? "Restarting Backend" | tee -a $LOG_FILE
 
 
 
